4f61590ff62b894c47d30d51956fc1b2f0ecf755
Mário Calegaro <mcalegaro@gmail.com>
2020-10-13 00:06:19 -0300

    build config

--- complete/src/main/java/com/example/restservice/RestServiceApplication.java
+++ complete/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -3,7 +3,7 @@ package com.example.restservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class RestServiceApplication {
 
     public static void main(String[] args) {
